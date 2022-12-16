60b52029f9e53637612c9098290f63a594fe8269
valosekj <jan.valosek@upol.cz>
2022-12-16 12:15:04 -0500

    Add script for processing of DCM patients

---
+++ bin/process_data_dcm.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+#
+# Run `sct_process_segmentation -normalize PAM50` on T2w images from DCM patients (twh dataset)
+# SC segmentation and discs from /derivatives are used
+# Note: files in /derivatives were created from "raw" T2w images (i.e., without any preprocessing steps). Thus, no
+# preprocessing steps are used also within this script.
+
+# Usage:
+#     sct_run_batch -c <PATH_TO_REPO>/etc/config_process_data.json
+
+# The following global variables are retrieved from the caller sct_run_batch
+# but could be overwritten by uncommenting the lines below:
+# PATH_DATA_PROCESSED="~/data_processed"
+# PATH_RESULTS="~/results"
+# PATH_LOG="~/log"
+# PATH_QC="~/qc"
+#
+# Authors: Jan Valosek, Sandrine Bedard, Julien Cohen-Adad
+#
+
+# Uncomment for full verbose
+set -x
+
+# Immediately exit if error
+set -e -o pipefail
+
+# Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
+trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
+
+# Print retrieved variables from the sct_run_batch script to the log (to allow easier debug)
+echo "Retrieved variables from from the caller sct_run_batch:"
+echo "PATH_DATA: ${PATH_DATA}"
+echo "PATH_DATA_PROCESSED: ${PATH_DATA_PROCESSED}"
+echo "PATH_RESULTS: ${PATH_RESULTS}"
+echo "PATH_LOG: ${PATH_LOG}"
+echo "PATH_QC: ${PATH_QC}"
+
+# CONVENIENCE FUNCTIONS
+# ======================================================================================================================
+# Check if manual spinal cord segmentation file already exists. If it does, copy it locally.
+# If it doesn't, perform automatic spinal cord segmentation
+segment_if_does_not_exist() {
+  local file="$1"
+  local contrast="$2"
+  # Update global variable with segmentation file name
+  FILESEG="${file}_seg"      # remove '_RPI_r' to match derivatives/labels files
+  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}-manual.nii.gz"
+  echo
+  echo "Looking for manual segmentation: $FILESEGMANUAL"
+  if [[ -e $FILESEGMANUAL ]]; then
+    echo "Found! Using manual segmentation."
+    rsync -avzh $FILESEGMANUAL ${FILESEG}.nii.gz
+    sct_qc -i ${file}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  else
+    echo "Not found. Proceeding with automatic segmentation."
+    # Segment spinal cord
+    sct_deepseg_sc -i ${file}.nii.gz -o ${FILESEG}.nii.gz -c ${contrast} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  fi
+}
+
+# Check if manual label already exists. If it does, generate labeled segmentation from manual disc labels.
+# If it doesn't, perform automatic spinal cord labeling
+label_if_does_not_exist(){
+  local file="$1"
+  local file_seg="$2"
+  local contrast="$3"
+  # Update global variable with segmentation file name
+  FILELABEL="${file}_labels-disc"
+  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
+  echo "Looking for manual label: $FILELABELMANUAL"
+  if [[ -e $FILELABELMANUAL ]]; then
+    echo "Found! Using manual labels."
+    rsync -avzh $FILELABELMANUAL ${FILELABEL}.nii.gz
+    # Generate labeled segmentation from manual disc labels
+    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -discfile ${FILELABEL}.nii.gz -c ${contrast} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  else
+    echo "Not found. Proceeding with automatic labeling."
+    # Generate labeled segmentation automatically (no manual disc labels provided)
+    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -c ${contrast} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  fi
+}
+
+# Retrieve input params and other params
+SUBJECT=$1
+
+# get starting time:
+start=`date +%s`
+
+# ------------------------------------------------------------------------------
+# SCRIPT STARTS HERE
+# ------------------------------------------------------------------------------
+# Display useful info for the log, such as SCT version, RAM and CPU cores available
+sct_check_dependencies -short
+
+# Go to folder where data will be copied and processed
+cd $PATH_DATA_PROCESSED
+
+# Copy source T2w images
+# Note: we use '/./' in order to include the sub-folder 'ses-0X'
+rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/${SUBJECT}_T2w.* .
+
+# Go to subject folder for source images
+cd ${SUBJECT}/anat
+
+# ------------------------------------------------------------------------------
+# T2w
+# ------------------------------------------------------------------------------
+# Define variables
+# We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
+file_t2="${SUBJECT//[\/]/_}"_T2w
+
+# Note: manual segmentations and disc labels located under /derivatives were created from "raw" images without any
+# preprocessing. Thus, no preprocessing steps are applied also here.
+
+# Make sure that input T2w image and segmentation dimensions are the same
+dims_im=$(sct_image -i ${file_t2}.nii.gz -header | grep dim | head -1 | awk '{print $3, $4, $5}')
+dims_seg=$(sct_image -i ${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${file_t2}_seg-manual.nii.gz -header | grep dim | head -1 | awk '{print $3, $4, $5}')
+
+if [[ ${dims_im} == ${dims_seg} ]];then
+    # Copy SC segmentation from /derivatives
+    segment_if_does_not_exist ${file_t2} 't2'
+    file_t2_seg=$FILESEG
+    # Create labeling from manual disc labels located at /derivatives
+    label_if_does_not_exist ${file_t2} ${file_t2_seg} 't2'
+
+    # Compute metrics from SC segmentation and normalize them to PAM50 (`-normalize PAM50` flag)
+    sct_process_segmentation -i ${file_t2_seg}.nii.gz -perslice 1 -vert 1:20 -vertfile ${file_t2_seg}_labeled.nii.gz -o ${PATH_RESULTS}/${file_t2}.csv -normalize PAM50
+else
+    echo "Input T2w image and manual SC segmentation have different dimensions!"
+    echo "T2w dims: ${dims_im}"
+    echo "seg dims: ${dims_seg}"
+fi
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
+
+# Display results (to easily compare integrity across SCT versions)
+end=`date +%s`
+runtime=$((end-start))
+echo
+echo "~~~"
+echo "SCT version: `sct_version`"
+echo "Ran on:      `uname -nsr`"
+echo "Duration:    $(($runtime / 3600))hrs $((($runtime / 60) % 60))min $(($runtime % 60))sec"
+echo "~~~"
